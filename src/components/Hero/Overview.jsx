aad96426bdc693f598d2ea50ad239b390539a5c2
Hassan-src <hassane2881@yahoo.com>
2026-07-30 21:03:00 +0330

    Update

--- src/components/Hero/Overview.jsx
+++ src/components/Hero/Overview.jsx
@@ -1,4 +1,4 @@
-import TextExpander from "../TextExpander/TextExpander";
+import TextExpander from "../TextExpander/textExpander";
 import styles from "./Overview.module.css";
 function Overview({ children }) {
   // Includes overview and using a custom textexpander function to shorten the text
